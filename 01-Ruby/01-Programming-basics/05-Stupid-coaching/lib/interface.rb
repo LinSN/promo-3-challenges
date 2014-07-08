adac2c6b1ea2c8f2035b6b2091169229e5fa3121
Nordine Slimani <slimaninordine@gmail.com>
2014-07-08 15:32:02 +0200

    Je pose une question tant que je donne pas la bonne reponse

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,7 +1,11 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
 
+
 puts "Que souhaitez-vous dire a votre coach"
 ask = gets.chomp
 
-puts coach_answer(ask)
+until coach_answer(ask) == ""
+  puts "Mauvaise Réponse --> Poser une nouvelle question"
+  ask = gets.chomp
+end
