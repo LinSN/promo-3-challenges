041fc2ca4e475b9486099760a2ae676f00d18a1e
Nordine Slimani <slimaninordine@gmail.com>
2014-07-09 15:18:21 +0200

    Solved

--- 01-Ruby/02-Arrays-Enumerable/05-Debug-Gym/lib/bugged_concatenate.rb
+++ 01-Ruby/02-Arrays-Enumerable/05-Debug-Gym/lib/bugged_concatenate.rb
@@ -1,7 +1,11 @@
-def bugged_concatenate(array)
-  array.inject("") { |output, element| output + element }.upcase
+def bugged_concatenate(*array)
+  ma_string = ""
+  array.each{ |output|  ma_string.insert(ma_string.size,"#{output.to_s}")  }
+  return ma_string.upcase
 end
 
 def build_1984_title
   bugged_concatenate(1, "9", 84, " ", "George Orwell")
 end
+
+puts bugged_concatenate(1, "9", 84, " ", "George Orwell")
