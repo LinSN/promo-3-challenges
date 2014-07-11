fcc2d70df1ff1394a225010f9a4b6b87df3f47aa
Nordine Slimani <slimaninordine@gmail.com>
2014-07-11 11:13:00 +0200

    solved

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,7 +1,51 @@
 def poor_calories_counter(burger, side, beverage)
+  sum=0
+  product_calories = {
+    "Cheese Burger"=> 290,
+    "Big Mac"=> 300,
+    "Mc Bacon"=> 400,
+    "Royal Cheese"=> 130,
+    "French fries"=> 130,
+    "Potatoes"=> 130,
+    "Coca"=> 160,
+    "Sprite"=> 170
+  }
+  product_calories[burger] + product_calories[side] + product_calories[beverage]
+
+
+
   #TODO: return number of calories for this mcDonald order
 end
 
 def calories_counter(*orders)
+  sum = 0
+  product_calories = {
+    "Cheese Burger"=> 290,
+    "Big Mac"=> 300,
+    "Mc Bacon"=> 400,
+    "Royal Cheese"=> 130,
+    "French fries"=> 130,
+    "Potatoes"=> 130,
+    "Coca"=> 160,
+    "Sprite"=> 170,
+    "Happy Meal" => 580,
+    "Best Of Big Mac" => 590,
+    "Best Of Royal Cheese" => 430
+
+  }
+
+
+  orders.each do|element|
+    product_calories.each do|key,value|
+      if element == key
+        sum += value
+      end
+    end
+  end
+  sum
   #TODO: return number of calories for a less constrained order
 end
+
+puts poor_calories_counter("Big Mac", "Potatoes", "Sprite")
+puts calories_counter("Mc Bacon", "Potatoes", "Sprite","Big Mac","Coca" )
+
