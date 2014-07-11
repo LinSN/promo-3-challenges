41633427a1a1575a1ff6d929510b817e9a8a1d2e
Nordine Slimani <slimaninordine@gmail.com>
2014-07-11 11:55:29 +0200

    moins de sensibilite a la casse

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,51 +1,51 @@
-def poor_calories_counter(burger, side, beverage)
-  sum=0
-  product_calories = {
-    "Cheese Burger"=> 290,
-    "Big Mac"=> 300,
-    "Mc Bacon"=> 400,
-    "Royal Cheese"=> 130,
-    "French fries"=> 130,
-    "Potatoes"=> 130,
-    "Coca"=> 160,
-    "Sprite"=> 170
-  }
-  product_calories[burger] + product_calories[side] + product_calories[beverage]
+ def poor_calories_counter(burger, side, beverage)
+    sum=0
+    product_calories = {
+     "Cheese Burger"=> 290,
+     "Big Mac"=> 300,
+     "Mc Bacon"=> 400,
+     "Royal Cheese"=> 130,
+     "French fries"=> 130,
+     "Potatoes"=> 130,
+     "Coca"=> 160,
+     "Sprite"=> 170
+   }
+    if product_calories.has_key?(burger) && product_calories.has_key?(side) && product_calories.has_key?(beverage)
+      product_calories[burger] + product_calories[side] + product_calories[beverage]
+    else
+      "Vous avez fait du caca dans l'appel de votre fonction"
+    end
 
 
-
-  #TODO: return number of calories for this mcDonald order
+    #TODO: return number of calories for this mcDonald order
 end
 
 def calories_counter(*orders)
-  sum = 0
-  product_calories = {
-    "Cheese Burger"=> 290,
-    "Big Mac"=> 300,
-    "Mc Bacon"=> 400,
-    "Royal Cheese"=> 130,
-    "French fries"=> 130,
-    "Potatoes"=> 130,
-    "Coca"=> 160,
-    "Sprite"=> 170,
-    "Happy Meal" => 580,
-    "Best Of Big Mac" => 590,
-    "Best Of Royal Cheese" => 430
+   sum = 0
+   product_calories = {
+     "Cheese Burger"=> 290,
+     "Big Mac"=> 300,
+     "Mc Bacon"=> 400,
+     "Royal Cheese"=> 130,
+     "French fries"=> 130,
+     "Potatoes"=> 130,
+     "Coca"=> 160,
+     "Sprite"=> 170,
+     "Happy Meal" => 580,
+     "Best Of Big Mac" => 590,
+     "Best Of Royal Cheese" => 430
 
-  }
+   }
 
 
-  orders.each do|element|
-    product_calories.each do|key,value|
-      if element == key
-        sum += value
-      end
-    end
-  end
-  sum
-  #TODO: return number of calories for a less constrained order
+   orders.each do|element|
+      product_calories.has_key?(element)
+      sum += product_calories[element]
+   end
+   sum
+   #TODO: return number of calories for a less constrained order
 end
 
-puts poor_calories_counter("Big Mac", "Potatoes", "Sprite")
-puts calories_counter("Mc Bacon", "Potatoes", "Sprite","Big Mac","Coca" )
+ puts poor_calories_counter("Big Mac", "Potatoes", "Sprite")
+ puts calories_counter("Mc Bacon", "Potatoes", "Sprite","Big Mac","Coca" )
 
