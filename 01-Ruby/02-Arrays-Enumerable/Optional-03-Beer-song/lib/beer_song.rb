321501fdc431610b94c4f9dce95708e742003b2b
Nordine Slimani <slimaninordine@gmail.com>
2014-07-14 17:18:04 +0200

    done

--- 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
@@ -1,3 +1,16 @@
-def beersong(nb_at_start)
-  #TODO: sing the song
-end
+#TODO: sing the song
+  nb_at_start = ARGV[0].to_i
+  if ARGV.empty?
+    puts "Pas d'argument"
+    exit
+  else
+    for i in (0..nb_at_start-1)
+      if nb_at_start-i > 1
+        puts "#{nb_at_start-i} bottles of beer on the wall, #{nb_at_start-i} bottles of beer!" + "\n"
+        puts "Take one down, pass it around, #{nb_at_start-i-1} bottles of beer on the wall!"
+      else
+        puts "1 bottle of beer on the wall, 1 bottle of beer!" + "\n"
+        puts "Take one down, pass it around, no more bottles of beer on the wall!"
+      end
+    end
+  end
