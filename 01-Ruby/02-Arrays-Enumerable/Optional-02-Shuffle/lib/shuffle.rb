0cdce02a8ed40df7f54536ad3a171f4f05a48dfc
Nordine Slimani <slimaninordine@gmail.com>
2014-07-15 01:10:34 +0200

    done both

--- 01-Ruby/02-Arrays-Enumerable/Optional-02-Shuffle/lib/shuffle.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-02-Shuffle/lib/shuffle.rb
@@ -1,3 +1,20 @@
 def shuffle(array)
   #TODO: shuffle array manually
+  array_solution = []
+  array_bis = array.clone
+
+  until array_bis.empty?
+    tmp = array_bis[rand(array_bis.size)]
+    array_solution << tmp
+    array_bis.delete(tmp)
+    print array
+  end
+  array_solution
+
 end
+
+def shuffle_plus(array)
+  array.sort_by{|value,index| rand(index)}
+end
+
+puts shuffle_plus([1,2,3,4,5,6])
