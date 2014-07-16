7b0e3cfd7e86131f3bc73cbfa174fad465bf31a6
Nordine Slimani <slimaninordine@gmail.com>
2014-07-16 16:40:03 +0200

    done

--- 02-OOP/01-OO-Basics/04-Public-Interface/lib/account.rb
+++ 02-OOP/01-OO-Basics/04-Public-Interface/lib/account.rb
@@ -1,9 +1,11 @@
 # This is how you define your own custom exception classes
+require_relative'transaction'
+
 class DepositError < StandardError
 end
 
 class BankAccount
-
+attr_reader :name,:position
   # Contract for the BankAccount class
   # - you can access full owner's name and position, but partial IBAN
   # - you cannot access full IBAN
@@ -27,25 +29,40 @@ class BankAccount
   def withdraw(amount)
     # TODO: Call add_transaction with the right argument
     # TODO: returns a string with a message
+    add_transaction(-amount)
+    " Your withdraw is: #{amount} /n Your current position is: #{@position}"
   end
 
   def deposit(amount)
     # TODO: Call add_transaction with the right argument
     # TODO: returns a string with a message
+    add_transaction(amount)
+    " Your deposit is: #{amount} /n Your current position is: #{@position}"
   end
 
   def transactions_history(args = {})
     # TODO: Check if there is a password and if so if it is correct
     # TODO: return a string displaying the transactions, BUT NOT return the transaction array !
+    if args[:password] == @password
+      @transactions.join(" ")
+    elsif args.empty?
+      'no password given'
+    else
+      'wrong password'
+    end
   end
 
   def iban
     # TODO: Hide the middle of the IBAN like FR14**************606 and return it
+
+    @iban[0..3] + "*"*(@iban.size-7) + @iban[-3..-1]
   end
 
   def to_s
     # Method used when printing account object as string (also used for string interpolation)
     # TODO: Displays the account owner, the hidden iban and the position of the account
+
+    "Account Owner: #{@name} /n IBAN:#{iban} /n Position: #{@position}"
   end
 
   private
@@ -53,6 +70,9 @@ class BankAccount
   def add_transaction(amount)
     # TODO: add the amount in the transactions array
     # TODO: update the current position (which represents the balance of the account)
+    @transactions << amount
+    @position += amount
+
   end
 
 end
--- 02-OOP/01-OO-Basics/04-Public-Interface/lib/transaction.rb
+++ 02-OOP/01-OO-Basics/04-Public-Interface/lib/transaction.rb
@@ -1,10 +1,14 @@
 # Optional
+
 class Transaction
   def initialize(amount)
     # Initialize amount and date of transaction
+    @amout = amount
+    @time = Time.now
   end
 
   def to_s
     # Nicely print transaction infos using Time#strftime.
+    @time.strftime("Transaction done on %m/%d/%Y at %I:%M%p")
   end
 end
