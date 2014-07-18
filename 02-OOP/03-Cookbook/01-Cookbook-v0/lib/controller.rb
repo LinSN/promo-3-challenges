16f31f9346673a7ad59ef9985155388397c777db
Nordine Slimani <slimaninordine@gmail.com>
2014-07-18 15:52:32 +0200

    pas fini

--- 02-OOP/03-Cookbook/01-Cookbook-v0/lib/controller.rb
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/lib/controller.rb
@@ -1,8 +1,15 @@
 require_relative 'cookbook'
+require_relative 'ui'
 
 class Controller
   def initialize(file)
     # Here you should instantiate the Cookbook model with the file
+    @cookbook = Cookbook.new('/Users/nordineslimani/code/LinSN/promo-3-challenges/02-OOP/03-Cookbook/01-Cookbook-v0/lib/recipes.csv')
+    @ui = UI.new
+  end
+
+  def display_receip
+    ui.display(cookbook.list_of_all_recipes)
   end
 
   # TODO: Implement the methods to retrieve, add, and delete recipes through the model
--- 02-OOP/03-Cookbook/01-Cookbook-v0/lib/cookbook.rb
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/lib/cookbook.rb
@@ -1,9 +1,53 @@
+require 'csv'
+
 class Cookbook
+  attr_reader :recipes
+
   def initialize(file)
     # TODO: Retrieve the data from your CSV file and store it in an instance variable
+    @recipes = []
+    @filepath    = file
+  end
+
+  def list_of_all_recipes
+    @recipes = []
+    CSV.foreach(@filepath) do |row|
+      @recipes << row[0]
+    end
+    @recipes
+  end
+
+  def add_recipes(name)
+    @recipes << name
+    CSV.open(@filepath, 'w') do |csv|
+      @recipes.each do |recipe|
+        csv << [recipe]
+      end
+    end
+  end
+
+  def delete_recipes(name)
+    @recipes.delete(name)
+    CSV.open(@filepath, 'w', @csv_options) do |csv|
+      @recipes.each do |recipe|
+        csv << [recipe]
+      end
+    end
   end
 
   # TODO: Implement the methods to retrieve all recipes, create, or destroy recipes
   # TODO: Implement a save method that will write the data into the CSV
   # And don't forget to use this save method when you have to modify something in your recipes array.
 end
+
+cookbook = Cookbook.new
+print cookbook.list_of_all_recipes
+cookbook.add_recipes("Tarte aux pommes")
+cookbook.add_recipes("cassolette")
+print cookbook.list_of_all_recipes
+
+cookbook.add_recipes("poule o pot")
+print cookbook.list_of_all_recipes
+
+cookbook.delete_recipes("poule o pot")
+print cookbook.list_of_all_recipes
