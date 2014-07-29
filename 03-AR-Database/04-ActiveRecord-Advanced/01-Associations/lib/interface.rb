a8b63f1d6188ed0e8ccd2ed1ac0597730c44b980
Nordine Slimani <slimaninordine@gmail.com>
2014-07-29 14:48:30 +0200

    Ddone

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/interface.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/lib/interface.rb
@@ -1,43 +1,63 @@
 require_relative 'config/application'
 require_relative 'models/post'
 require_relative 'models/user'
+require 'colorize'
 
+User.where(name: "Nordine", email: "nono@nono.com").first_or_create
 
-def ask_and_get(param)
-  puts "What is the #{param} of your piece of news?"
-  gets.chomp
+def ask(prompt)
+  print "#{prompt} "
+  gets.to_s.chomp
 end
 
-while true
+def create_post(user)
+  p user.posts
+  name = ask('Name:')
+  source_url = ask('Source URL:')
+  rating = ask('Rating:').to_i
+  post_params = { name: name, source_url: source_url, date: Time.now, rating: rating }
 
-  logged_in = false
+  # TODO: use ActiveRecord to add a new post for the user logged in!
 
-  until logged_in
-    puts "Please login with your <id>"
-    #TODO: instantiate a user with his <id>
+  user.posts.create(post_params)
+end
+
+def list_posts(user)
+  # TODO: use ActiveRecord to get all posts of the current user
+  posts = user.posts.all
+  posts.each do |post|
+    puts '%-3s %-15s %-20s %-30s %s' % [post.id.to_s.green, post.name, post.source_url, post.date, post.rating.to_s.blue]
   end
+end
+
+def delete_posts(user)
+  # TODO: use ActiveRecord to delete all posts of current user
+  user.posts.delete_all
+end
+
+logged_in = false
+until logged_in
+  puts 'Please login with your <id>'
+  # TODO: instantiate a user with his <id>
+  id = gets.chomp
+  user = User.find(id)
+
+  logged_in = true
+
+end
+
+while logged_in
 
   puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
   puts "1. Create a post"
   puts "2. Read your posts"
   puts "3. Delete all posts"
   puts "4. Exit"
-  
-	choice =  gets.chomp.to_i
-	
-	case choice
-  when 1
-    name = ask_and_get("name")
-    source_url = ask_and_get("source url")
-    rating = ask_and_get("rating")
-    post = { name: name, source_url: source_url, date: Time.now, rating: rating }
-    #TODO: use ActiveRecord to add a new post for the user logged in!
-  when 2
-    #TODO: use ActiveRecord to get all posts of the current user
-  when 3
-    #TODO: use ActiveRecord to delete all posts of current user 
-  when 4 
-    break
-	end 
-	
+
+  case ask('>').to_i
+  when 1 then create_post(user)
+  when 2 then list_posts(user)
+  when 3 then delete_posts(user)
+  when 4 then break
+  end
 end
