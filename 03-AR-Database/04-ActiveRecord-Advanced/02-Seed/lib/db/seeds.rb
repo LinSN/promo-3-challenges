72bbdc8f2a73ede848d27e8ee66cfc8465afad65
Nordine Slimani <slimaninordine@gmail.com>
2014-07-30 12:12:46 +0200

    modif

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Seed/lib/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Seed/lib/db/seeds.rb
@@ -11,3 +11,24 @@ puts 'Seeding database...'
 # TODO: Your code goes here
 # 1. create fake users
 # 2. Scrape news from https://news.ycombinator.com/ and associate posts to your existing users
+User.delete_all
+TheOne = User.where(name: Faker::Name.name, email: Faker::Internet.email).first_or_create
+TheSecond = User.where(name: Faker::Name.name, email: Faker::Internet.email).first_or_create
+TheThird = User.where(name: Faker::Name.name, email: Faker::Internet.email).first_or_create
+
+doc = Nokogiri::HTML(open("https://news.ycombinator.com/news"))
+
+posts = {}
+ratings = []
+
+doc.search('table table tr td.title a').each do |element|
+  next if element.inner_text == 'More'
+  posts[element.inner_text] = element.xpath('@href').first.value
+end
+
+doc.search('table table tr td.subtext').each do |element|
+  ratings << (element.search('span').inner_text[0].to_i || 0)
+end
+
+p posts
+p ratings
