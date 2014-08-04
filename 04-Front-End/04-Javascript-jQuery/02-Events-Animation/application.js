ced8ae1a9c3c15e2725bab94fb05ca7d4319512a
Nordine Slimani <slimaninordine@gmail.com>
2014-08-04 16:28:30 +0200

    done

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,12 @@
   * TODO: add your code here!
   *
 */
+$('#message-wrapper').css("top","-45px");
+
+$('#message-wrapper').animate({
+  top: "0px"
+},1500);
+
+$( "#message-wrapper" ).click(function() {
+  $( "#message" ).slideUp(1500)
+});
