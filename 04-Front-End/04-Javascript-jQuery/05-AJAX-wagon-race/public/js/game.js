c3b9c8d821143e2645d5ccfe7a80893780b915ce
Nordine Slimani <slimaninordine@gmail.com>
2014-08-07 15:03:36 +0200

    done

--- 04-Front-End/04-Javascript-jQuery/05-AJAX-wagon-race/public/js/game.js
+++ 04-Front-End/04-Javascript-jQuery/05-AJAX-wagon-race/public/js/game.js
@@ -1,87 +1,102 @@
 var GRID_SIZE = 30;
 var PLAYER1_KEY = 68; // D
 var PLAYER2_KEY = 76; // L
+var game_id;
+var session_id;
+$(function(){
+  $.get( "/session/create", function(data) { // session created
+    console.log(data);
+    session_id = data.id; //
+  }); //
 
-$(function() {
-  var session;
-
-  $.get( "http://localhost:4567/session/create", function(event) {
-    $('#start-button').removeAttr('hidden');
-    session = event;
+  $('form').hide();
+  $('table').hide();
+  $('#restart').hide();
+  $('#submit2').click(function(){
+    $('form').fadeIn();
   });
 
-  $('#start-button').click(function() {
-    $('#form-name').removeAttr('hidden');
-  });
 
-  $('#form-name').on('submit',function(e) {
-    e.preventDefault();
-    console.log(e);
-    var p1 = $("#player1-name").val();
-    var p2 = $("#player2-name").val();
+  $('#form').on('submit', function(event){
+    var time_ini = event.timeStamp;
+    event.preventDefault();
+  //   var player1 = $('#player1').val(); // Keep player1 value
+  //   var player2 = $('#player2').val(); // Keep player2 value
 
-    var data = JSON.stringify({
-                players: [
-                  { name: p1},
-                  { name: p2 }
-                ]
-              });
+  //   var rowsNumber = $('#rows_number').val(); //Keep rowsNumber
+    var player1 = $('#player1').val();
+    var player2 = $('#player2').val();
+    var rowsNumber = $('#rows_number').val();
+    $('form').fadeOut(300);
+    $('#submit2').fadeOut(300);
+    $('table').delay(1000).fadeIn(300);
 
-    $.post( "http://localhost:4567/session/" + session.id + "/game/create", data, function(argument) {
 
-      console.log(argument.players);
 
-      $('#start-button').attr("hidden","true")
-      $('#form-name').attr("hidden","true")
-
-      generate_grid(GRID_SIZE);
+    var data_yo = JSON.stringify( {
+        "players": [
+          {'name': player1},
+          {'name': player2}
+        ]
+      })
+    $.post( '/session/' + session_id + '/game/create', data_yo,
+      function(json) {
+      var game_id = json.game.id;
+      generate_grid(rowsNumber);
       give_player_hints(PLAYER1_KEY, PLAYER2_KEY);
+        var player1id = json.game.players[0].id;
+        var player2id = json.game.players[1].id;
+        console.log(player1id);
+        console.log(player2id);
 
-      $(document).on('keyup', function(event) {
-
-        // d = 68
-        // l = 76
-        if (event.which == PLAYER1_KEY) {
-            update_player_position("player1", 1, p1);
-        }
-        if (event.which == PLAYER2_KEY) {
-            update_player_position("player2", 1, p2);
-        }
-      });
+        $(document).on('keyup', function(event) {
+          // d = 68
+          // l = 76
+          if (event.which == PLAYER1_KEY) {
+              update_player_position("player1", 1, player1id, game_id, time_ini);
+          }
+          if (event.which == PLAYER2_KEY) {
+              update_player_position("player2", 1, player2id, game_id, time_ini);
+          }
     });
 
-  });
+  })
+})
+function update_player_position(player, number, playerid, game_id,time_ini) {
+  var cases  = $('#' + player + '_race td'),
+      length = cases.length,
+      previous_position = cases.siblings('.active').index();
 
-});
+  cases.removeClass('active');
+  cases.eq(previous_position + number).addClass('active');
 
+  console.log(previous_position + number, length);
+  if ((previous_position + number) >= length) {
+    alert(player + ' has won !');
 
+    var data = JSON.stringify({
+      "winner": playerid,
+      "elapsed_time": (new Date).getTime() - time_ini
+    });
 
+    $.post('/game/' + game_id + '/finish', data, function(json){
+      console.log(json);
+    });
+    $.get('/game/' + game_id + '/results', function(data){
+      console.log(data);
+    })
 
+    $('#restart').fadeIn(30).click(function(){
+      reset();
+      $(this).fadeOut();
+    });
 
-
-
-
-
-
-
-
-function update_player_position(player, number, playername) {
-    var cases  = $('#' + player + '_race td'),
-        length = cases.length,
-        previous_position = cases.siblings('.active').index();
-
-    cases.removeClass('active');
-    cases.eq(previous_position + number).addClass('active');
-
-    console.log(previous_position + number, length);
-    if ((previous_position + number) >= length) {
-        alert(playername + ' has won !');
-        reset();
-    }
+  }
 }
 
+})
+
 function generate_grid(size) {
-  $('body').append('<table class="racer_table"><tr id="player1_race"><td class="active"></td></tr> <tr id="player2_race"><td class="active"></td></tr></table>')
   for (var i = 0; i < size - 1; i++) {
     $('#player1_race').append('<td></td>');
     $('#player2_race').append('<td></td>');
